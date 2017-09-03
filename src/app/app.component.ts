21fc376b8d9c4658f34ff1cf43475698adad2df4
Martin Hochel <hochelmartin@gmail.com>
2017-09-03 14:35:16 +0200

    feat(app): add styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ interface Todo {
   selector: 'app-root',
   template: `
     <h1>
-      Welcome to {{title}}!!
+      {{title}}
     </h1>
     <app-input (addTodo)="handleTodoAdd($event)"></app-input>
     <ul>
@@ -18,10 +18,66 @@ interface Todo {
       </li>
     </ul>
   `,
-  styles: [],
+  styles: [
+    `
+  :host {
+    display: block;
+    position: relative;
+    width: 400px;
+    padding: 30px 30px 15px;
+    background: white;
+    border: 1px solid;
+    border-color: #dfdcdc #d9d6d6 #ccc;
+    border-radius: 2px;
+    -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    margin: 20px auto;
+  }
+
+  :host:before, :host:after {
+    content: '';
+    position: absolute;
+    z-index: -1;
+    height: 4px;
+    background: white;
+    border: 1px solid #ccc;
+    border-radius: 2px;
+  }
+
+  :host:after {
+    bottom: -3px;
+    left: 0;
+    right: 0;
+    -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.1);
+  }
+
+  :host:before {
+    bottom: -5px;
+    left: 2px;
+    right: 2px;
+    border-color: #c4c4c4;
+    -webkit-box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+  }
+
+  :host h1 {
+    font-size: 52px;
+    line-height: 52px;
+    margin-bottom: 30px;
+    font-weight: bold;
+    text-align: center;
+    letter-spacing: -0.8px;
+  }
+
+  app-input {
+    margin-bottom: 20px;
+  }
+  `,
+  ],
 })
 export class AppComponent {
-  title = 'TodoList App'
+  title = 'My Todos'
   todoList = [
     { title: 'install NodeJS' },
     { title: 'install Angular CLI' },
